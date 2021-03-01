33aee83f7d0c6a6d177531d44b455c2a98b7cca4
Anja Tomić <anja.tomic099@gmail.com>
2021-03-01 17:50:53 +0100

    change route for home

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/home" exact  component={Home}/>
-          <Route path="/services"   component={Services}/>
-          <Route path="/products"   component={Products}/>
-          <Route path="/sign-up"   component={SignUp}/>
+          <Route path="/" exact  component={Home} />
+          <Route path="/services"   component={Services} />
+          <Route path="/products"   component={Products} />
+          <Route path="/sign-up"   component={SignUp} />
         </Switch>
       </Router>
     </>
